7f5af6a755d12084abd3fb4cff1d45da76fbc27d
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-12-28 22:51:30 +0100

    Simplified Passport/Passport-Local Configuration

--- app.js
+++ app.js
@@ -8,24 +8,42 @@ const session = require('express-session');
 const passport= require('passport');
 const passportLocalMongoose= require('passport-local-mongoose');
 
-
-mongoose.connect('mongodb://localhost:27017/userSecretDB',{useNewUrlParser: true});
-
-
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use(session({
+    secret: 'Our little secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true }
+  }))
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://localhost:27017/userSecretDB',{useNewUrlParser: true});
+
+
+
 const userSchema =new mongoose.Schema({
     email:String,
     password: String
 });
 
-
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User",userSchema);
+
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
     res.render("home");
@@ -38,11 +56,31 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 
-app.post("/register",function(req,res){
-
-
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated){
+        res.render("/secrets");
+    }
+    else{
+        res.redirect("/login");
+    }
 });
 
+
+app.post("/register",function(req,res){
+ 
+    User.register({username: req.body.username, active: false}, req.body.password, function(err, user) {
+        if (!err) { 
+            res.redirect("register");
+         }
+      
+         else{
+            passport.authenticate("local")(req,res, function() {
+            res.redirect("/secrets");
+                // Value 'result' is set to false. The user could not be authenticated since the user is not active
+              });
+         }
+      });
+});
 
 app.post("/login",function(req,res){
 
