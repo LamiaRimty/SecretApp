0f9302b2345b3d3e4d5bbb8afc560e586d8c7d15
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-02 17:22:42 +0100

    foundUsers with secrets

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ const userSchema =new mongoose.Schema({
     email:String,
     password: String,
     googleId:String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 
 });
 
@@ -129,12 +130,17 @@ app.get("/register",function(req,res){
 });
 
 app.get("/secrets",function(req,res){
-    if(req.isAuthenticated){
-        res.render("secrets");
-    }
-    else{
-        res.redirect("/login");
-    }
+    User.find({ "secret": { "$ne": 'null' } }, function(err,foundUsers){
+        if(err){
+            console.log(err);
+        }
+
+        else{
+            if(foundUsers){
+                res.render("secrets",{ userWithSecret: foundUsers })
+            }
+        }
+    });
 });
 
 
@@ -195,6 +201,23 @@ app.post("/login",function(req,res){
  });
 });
 
+app.post("/submit",function(req,res){
+    const submittedSecret= req.body.secret;
+    console.log(req.user.id);
+    User.findById(req.user.id,function(err,foundUser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                foundUser.secret=submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
 
 
 app.listen(3000,function(){
